1b142337f622b2df6db4c5eb5ee73ac108f9f292
Stefan Forstenlechner <stefanforstenlechner@gmail.com>
2015-09-15 16:25:00 +0100

    add command line arguments
    define repetitions with the argument -r
    the file is saved every repetition

---
+++ HeuristicLab.ConsoleApplication/Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace HeuristicLab.ConsoleApplication {
+  public class Options {
+    //[Option('e', "experiment", Required = true,
+    //  HelpText = "File which contains the experiment which should be run (Algorithm, BatchRun, Experiment).")]
+    [ValueOption(0)]
+    public string InputFile { get; set; }
+
+    [Option('r', "repetitions", Required = false, DefaultValue = 1,
+      HelpText = "Number of repetitions")]
+    public int Repetitions { get; set; }
+
+    [Option('v', "verbose", DefaultValue = true,
+      HelpText = "Prints all messages to standard output.")]
+    public bool Verbose { get; set; }
+
+    [ParserState]
+    public IParserState LastParserState { get; set; }
+
+    [HelpOption]
+    public string GetUsage() {
+      return HelpText.AutoBuild(this,
+        (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+    }
+  }
+}
--- HeuristicLab.ConsoleApplication/Program.cs
+++ HeuristicLab.ConsoleApplication/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Threading;
 using HeuristicLab.Common;
 using HeuristicLab.Core;
@@ -10,25 +9,58 @@ namespace HeuristicLab.ConsoleApplication {
 
     private static ManualResetEventSlim eventHandle = new ManualResetEventSlim(false, 1);
     public static void Main(string[] args) {
-      if (args.Length < 1 || !File.Exists(args[0])) {
-        Console.WriteLine("First argument has to be a file path");
-        return;
+      try {
+        var options = new Options();
+        if (CommandLine.Parser.Default.ParseArguments(args, options)) {
+          if (options.Verbose) {
+            Console.WriteLine("Filename: {0}", options.InputFile);
+            Console.WriteLine("Repetitions: {0}", options.Repetitions);
+          }
+
+          Program p = new Program(options.InputFile, options.Repetitions, options.Verbose);
+          if (!p.Load()) {
+            Console.WriteLine("File could not be loaded.");
+            return;
+          }
+          for (int i = 0; i < options.Repetitions; i++) {
+            Console.WriteLine("rep: " + i);
+            p.Start();
+            eventHandle.Wait();
+            eventHandle.Reset();
+            p.Save();
+          }
+        } else {
+          Console.WriteLine(options.GetUsage());
+        }
       }
-      string filePath = args[0];
-      Program p = new Program(filePath);
-      p.Start();
-      eventHandle.Wait();
-      p.SaveAndExit();
+      catch (Exception e) {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
+      }
     }
+
     private IStorableContent content;
     private IOptimizer optimizer;
     private int numberOfRuns;
     private string filePath;
-    public Program(string filePath) {
+    private int initialNumberOfRuns;
+    private int repetitons;
+    private bool verbose;
+    private bool loaded = false;
+
+    public Program(string filePath, int repetitons, bool verbose = false) {
       this.filePath = filePath;
+      this.repetitons = repetitons;
+      this.verbose = verbose;
+
+      Console.Clear();
     }
 
-    public void Start() {
+    public bool Load() {
+      if (loaded) {
+        Console.WriteLine("A file has already been loaded.");
+        return false;
+      }
       ContentManager.Initialize(new PersistenceContentManager());
       content = ContentManager.Load(filePath);
 
@@ -38,22 +70,32 @@ namespace HeuristicLab.ConsoleApplication {
       optimizer = content as IOptimizer;
       if (optimizer != null) {
         numberOfRuns = NumberOfRuns(optimizer);
+        initialNumberOfRuns = optimizer.Runs.Count;
+        Console.WriteLine(String.Format("Initial number of runs: {0}", initialNumberOfRuns));
         PrintRuns();
 
         optimizer.ExceptionOccurred += new EventHandler<EventArgs<Exception>>(Optimizer_Exception);
         optimizer.ExecutionStateChanged += new EventHandler(Optimizer_ExecutionStateChanged);
         optimizer.Stopped += new EventHandler(Optimizer_Stopped);
         optimizer.ExecutionTimeChanged += new EventHandler(Optimizer_ExecutionTimeChanged);
-        optimizer.Runs.UpdateOfRunsInProgressChanged += new EventHandler(Optimizer_Runs_UpdateOfRunsInProgressChanged);
+        optimizer.Runs.RowsChanged += new EventHandler(Optimizer_Runs_RowsChanged);
+      }
+      loaded = optimizer != null;
+      return loaded;
+    }
 
+    public void Start() {
+      if (!loaded) {
+        Console.WriteLine("Nothing has been loaded. Call Load() before Start().");
+        return;
+      }
+      if (optimizer.ExecutionState != ExecutionState.Started) {
         optimizer.Prepare();
         optimizer.Start();
-      } else {
-        Console.WriteLine("Unknown content in file: " + content.ToString());
       }
     }
 
-    public void SaveAndExit() {
+    public void Save() {
       if (content != null) {
         Console.WriteLine("Saving...");
         ContentManager.Save(content, filePath, false);
@@ -64,33 +106,42 @@ namespace HeuristicLab.ConsoleApplication {
 
     }
 
+    private TimeSpan diffHour = new TimeSpan(1, 0, 0); // one hour
+    private TimeSpan diffMinute = new TimeSpan(0, 1, 0); // one minute
+    private TimeSpan last = TimeSpan.Zero;
+
     private void Optimizer_ExecutionTimeChanged(object sender, EventArgs e) {
-      Console.SetCursorPosition(0, 2);
-      Console.WriteLine(optimizer.ExecutionTime);
+      if ((verbose && optimizer.ExecutionTime.Subtract(last) > diffMinute)
+        || (!verbose && optimizer.ExecutionTime.Subtract(last) > diffHour)) {
+        //Console.SetCursorPosition(0, 2);
+        Console.WriteLine(optimizer.ExecutionTime);
+
+        last = optimizer.ExecutionTime;
+      }
     }
 
-    private void Optimizer_Runs_UpdateOfRunsInProgressChanged(object sender, EventArgs e) {
+    private void Optimizer_Runs_RowsChanged(object sender, EventArgs e) {
       PrintRuns();
     }
 
     private void PrintRuns() {
-      Console.SetCursorPosition(0, 3);
-      Console.WriteLine(String.Format("Runs: {0}/{1}", optimizer.Runs.Count, numberOfRuns));
+      //Console.SetCursorPosition(0, 3);
+      Console.WriteLine(String.Format("Number of Runs: {0}/{1}; Initial number of runs: {2}", optimizer.Runs.Count - initialNumberOfRuns, numberOfRuns * repetitons, initialNumberOfRuns));
     }
 
     private void Optimizer_ExecutionStateChanged(object sender, EventArgs e) {
-      Console.SetCursorPosition(0, 4);
+      //Console.SetCursorPosition(0, 4);
       Console.WriteLine(optimizer.ExecutionState);
     }
 
     private void Optimizer_Stopped(object sender, EventArgs e) {
-      Console.SetCursorPosition(0, 5);
+      //Console.SetCursorPosition(0, 5);
       Console.WriteLine("Finished.");
       eventHandle.Set();
     }
 
     private void Optimizer_Exception(object sender, EventArgs<Exception> e) {
-      Console.SetCursorPosition(0, 6);
+      //Console.SetCursorPosition(0, 6);
       Console.WriteLine("Optimizer Exception");
       Console.WriteLine(e.Value.Message);
       Console.WriteLine(e.Value.StackTrace);
